2b74bf5c3c83d550868c729a1a4967f47d8211e0
Marta Santos <mdpesa@gmail.com>
2023-01-30 23:34:21 +0100

    add components: Card and Card Form

---
+++ src/components/ColumnForm/ColumnForm.js
@@ -0,0 +1,35 @@
+import styles from './ColumnForm.module.scss';
+import Button from '../Button/Button';
+import { useState } from 'react';
+
+const ColumnForm = (props) => {
+  const [title, setTitle] = useState('');
+  const [icon, setIcon] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.action({ title: title, icon: icon });
+    setTitle('');
+    setIcon('');
+  };
+  return (
+    <form className={styles.columnForm} onSubmit={handleSubmit}>
+      Title:{' '}
+      <input
+        className={styles.input}
+        type='text'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      Icon:{' '}
+      <input
+        className={styles.input}
+        type='text'
+        value={icon}
+        onChange={(e) => setIcon(e.target.value)}
+      />
+      <Button>Add column</Button>
+    </form>
+  );
+};
+
+export default ColumnForm;
