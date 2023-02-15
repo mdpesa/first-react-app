9441cd03ec679ee898d35c72db89120aff73164e
Marta Santos <mdpesa@gmail.com>
2023-02-15 11:58:59 +0100

    strContains correction

--- src/utils/strContains.js
+++ src/utils/strContains.js
@@ -1,7 +1,5 @@
 export const strContains = (str1, str2) => {
-  if (str1.toLowerCase().includes(str2.toLowerCase())) {
-    return true;
-  } else return false;
+  return str1.toLowerCase().includes(str2.toLowerCase());
 };
 
 export default strContains;
