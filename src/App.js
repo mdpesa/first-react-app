b0c4636f4e76930a4530bba95cf25f0fdb5cc7b8
Marta Santos <mdpesa@gmail.com>
2023-02-15 16:48:38 +0100

    add Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import Container from './components/Container/Container.js';
 import NavBar from './components/NavBar/NavBar.js';
 import Home from './components/Home/Home.js';
+import About from './components/About/About.js';
+import Favorite from './components/Favorite/Favorite.js';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Container>
-        <Home />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/favorite' element={<Favorite />} />
+        </Routes>
       </Container>
     </main>
   );
