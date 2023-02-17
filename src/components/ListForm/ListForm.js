06ff7bb3a89d088a67ca960ddd027cddbace8bd5
Marta Santos <mdpesa@gmail.com>
2023-02-17 21:37:28 +0100

    Add ListForm component and add new list posibility

---
+++ src/components/ListForm/ListForm.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addList } from '../../redux/store';
+import TextInput from '../TextInput/TextInput';
+import styles from './ListForm.module.scss';
+import Button
+ from '../Button/Button';
+
+const ListForm = (props) => {
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addList({ title, description }));
+    setTitle('');
+    setDescription('');
+  };
+
+  return (
+    <form className={styles.listForm} onSubmit={handleSubmit}>
+      <div>
+        <span>Title:</span>
+        <TextInput
+          type='text'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        <span>Description:</span>
+        <TextInput
+          type='text'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </div>
+      <Button>Add list</Button>
+    </form>
+  );
+};
+
+export default ListForm;
--- src/redux/store.js
+++ src/redux/store.js
@@ -30,6 +30,8 @@ export const addColumn = (payload) => ({ type: 'ADD_COLUMN', payload });
 
 export const addCard = (payload) => ({ type: 'ADD_CARD', payload });
 
+export const addList = (payload) => ({ type: 'ADD_LIST', payload });
+
 export const updateSearchstring = (payload) => ({
   type: 'UPDATE_SEARCHSTRING',
   payload,
@@ -46,6 +48,11 @@ const reducer = (state, action) => {
       return {
         ...state,
         cards: [...state.cards, { id: shortid(), ...action.payload }],
+      };
+    case 'ADD_LIST':
+      return {
+        ...state,
+        lists: [...state.lists, { id: shortid(), ...action.payload }],
       };
     case 'UPDATE_SEARCHSTRING':
       return { ...state, searchString: action.payload };
